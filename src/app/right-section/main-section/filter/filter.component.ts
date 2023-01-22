d2232378fd3cf293ec09d2e361ffd62c81fcfd84
BotrosKamal <botros.kamal99@gmail.com>
2023-01-23 01:28:14 +0200

    adding custom directive to radio buttons div

--- src/app/right-section/main-section/filter/filter.component.ts
+++ src/app/right-section/main-section/filter/filter.component.ts
@@ -11,10 +11,18 @@ export class FilterComponent {
   @ViewChild('statusSelect', { static: true }) statusSelect: ElementRef;
   @ViewChild('typeSelect', { static: true }) typeSelect: ElementRef;
 
+  radioBtnValue: string = 'All';
+
   constructor() {
   }
 
   onChangeFilter(event: Event) {
-    this.filterSpecs.emit({ status: this.statusSelect.nativeElement.value, type: this.typeSelect.nativeElement.value })
+    // this.filterSpecs.emit({ status: this.statusSelect.nativeElement.value, type: this.typeSelect.nativeElement.value })
+    this.filterSpecs.emit({ status: this.radioBtnValue, type: this.typeSelect.nativeElement.value });
+  }
+
+  onChangeRadio(event: Event) {
+    this.radioBtnValue = (<HTMLInputElement>event.target).value;
+    this.onChangeFilter(event);
   }
 }
