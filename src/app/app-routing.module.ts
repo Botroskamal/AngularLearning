b2cad1599b633dc491c5e662c272fe321857048e
BotrosKamal <botros.kamal99@gmail.com>
2023-01-26 00:00:23 +0200

    create canDeactivateGuard service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SigninComponent } from './signin/signin.component';
+import { CanDeactivateGaurd } from './signup/can-deactivate-guard.service';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'signin', component: SigninComponent },
-  { path: 'signup', component: SignupComponent },
+  { path: 'signup', component: SignupComponent, canDeactivate: [CanDeactivateGaurd] },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'assets', component: DashboardComponent },
   { path: 'booking', component: DashboardComponent },
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements CanComponentDeactivate {
 
 }
