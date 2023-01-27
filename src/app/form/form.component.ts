869124ffb232d98c135aaf96be831ae19f154f32
BotrosKamal <botros.kamal99@gmail.com>
2023-01-27 21:12:31 +0200

    adding age validations

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { AccountsService } from '../services/accounts.service';
 
 @Component({
@@ -34,18 +34,51 @@ export class FormComponent implements OnInit {
 
     ngOnInit(): void {
         this.form = new FormGroup({
-            'first name': new FormControl(null),
-            'last name': new FormControl(null),
-            'email': new FormControl(null),
-            'password': new FormControl(null),
-            'confirm password': new FormControl(null),
-            'gender': new FormControl(null),
-            'age': new FormControl(null)
+            'first name': new FormControl(null, Validators.required),
+            'last name': new FormControl(null, Validators.required),
+            'email': new FormControl(null, [Validators.required, Validators.email]),
+            // 'password': new FormControl(null, [Validators.required, this.passwordValidation.bind(this)]),
+            'password': new FormControl(null, Validators.required),
+            'confirm password': new FormControl(null, Validators.required),
+            'gender': new FormControl(null, Validators.required),
+            'age': new FormControl(null, [Validators.required, this.ageValidation.bind(this)])
         });
     }
 
+    // passwordValidation(control: FormControl): {} {
+    //     const oneUpperCase = /(?=.*?[A-Z])/;
+    //     const oneLowerCase = /(?=.*?[a-z])/;
+    //     const oneDigit = /(?=.*?[0-9])/;
+    //     const specialChar = /(?=.*?[#?!@$%^&*-])/;
+    //     const minEight = /.{8,}/;
+    //     let errors: {} = {};
+    //     if (!oneUpperCase.test(control.value)) {
+    //         errors = { ...errors, 'noUpperCase': true }
+    //     }
+    //     if (!oneLowerCase.test(control.value)) {
+    //         errors = { ...errors, 'noLowerCase': true }
+    //     }
+    //     if (!oneDigit.test(control.value)) {
+    //         errors = { ...errors, 'noDigit': true }
+    //     }
+    //     if (!specialChar.test(control.value)) {
+    //         errors = { ...errors, 'noSpecialChar': true }
+    //     }
+    //     if (!minEight.test(control.value)) {
+    //         errors = { ...errors, 'lessThanEight': true }
+    //     }
+    //     return errors;
+    // }
+
+    ageValidation(control: FormControl): { [s: string]: boolean } {
+        if (control.value <= 16 && control.value !== null && control.value !== "") {
+            return { 'ageIsForbidden': true }
+        }
+        return null;
+    }
+
     onSubmit() {
-        console.log(this.form);
+        console.log(this.form.controls);
         // this.acountService.addAccount({
         //     id: 1,
         //     firstName: this.firstName,
