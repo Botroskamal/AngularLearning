5f03be9fb08aff5e70b0018370db6474e4e4248a
BotrosKamal <botros.kamal99@gmail.com>
2023-02-01 23:17:42 +0200

    add updateAccount to accountService

--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs";
 
 import { Account } from "./account.model";
 
@@ -17,12 +18,30 @@ export class AccountsService {
     }
 
     getAccounts() {
-        return this.http.get<{ [users: string]: Account[] }>('http://localhost:3000/users');
+        return this.http.get<{ [users: string]: Account[] }>('http://localhost:3000/users',
+            {
+                headers: new HttpHeaders({ 'hello': 'hi' }),
+                responseType: 'json'
+            }
+        ).pipe(
+            map(res => {
+                return res['users'];
+            })
+        );
     }
 
     deleteAccount(id: string) {
-        return this.http.post(`http://localhost:3000/delete/${id}`, {});
+        return this.http.post(`http://localhost:3000/delete/${id}`, {},
+            {
+                responseType: 'json'
+            });
     }
 
     // Update Account
+    updateAccount(id: string, data: {}) {
+        return this.http.put(`http://localhost:3000/update/${id}`, data,
+            {
+                responseType: 'json'
+            });
+    }
 }
